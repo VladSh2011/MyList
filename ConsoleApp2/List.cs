c9d67a3f1d741d17853346117d46076653133c95
VladSh2011 <129531386+VladSh2011@users.noreply.github.com>
2024-01-19 18:55:52 +0200

    Add project files.

---
+++ ConsoleApp2/List.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    public class MyList<T>
+    {
+        private T[] _list;
+        private int _count;
+        public MyList()
+        {
+            _list = new T[0];
+            _count = 0;
+        }
+
+        public void Clear()
+        {
+            _list = new T[0];
+            _count = 0;
+        }
+        //Прибрати дублювання коду
+        public void Add(T item)
+        {
+            if (_count == 0)
+            {
+                _count++;
+                _list = new T[_count];
+                _list[0] = item;
+            }
+            else
+            {
+                T[] tmp = new T[_count + 1];
+                for (int i = 0; i < _count; i++)
+                {
+                    tmp[i] = _list[i];
+                }
+                tmp[_count] = item;
+                _count++;
+                _list = tmp;
+            }
+        }
+        public void Show()
+        {
+            foreach (T item in _list)
+            {
+                Console.Write(item + "\t");
+            }
+            Console.WriteLine();
+        }
+        public bool Remove(T item)
+        {
+            if(!Contains(item))
+            {
+                return false;
+            }
+            for(int i = 0; i < _count; i++)
+            {
+                
+            }
+            return true;
+        }
+        public bool Contains(T? item)
+        {
+            if (item == null)
+            {
+                return false;
+            }
+            for(int i = 0; i < _count; i++)
+            {
+                if (item.Equals(_list[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public int Count => _count;
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyList<int> list = new MyList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        Console.WriteLine(list.Count);
+        list.Show();
+        //list.Clear();
+        //Console.WriteLine(list.Count);
+        //list.Show();
+        Console.WriteLine(list.Contains(2));
+        Console.WriteLine(list.Contains(5));
+    }
+}
+
