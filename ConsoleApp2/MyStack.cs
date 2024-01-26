80a408c43950ad272246006b3746516de9f15d03
VladSh2011 <129531386+VladSh2011@users.noreply.github.com>
2024-01-26 18:33:20 +0200

    Added MyStack

---
+++ ConsoleApp2/MyStack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class MyStack<T>
+    {
+        private MyList<T> _stack;
+        public MyStack() 
+        {
+            _stack = new MyList<T>();
+        }
+        public void Clear() => _stack.Clear();
+        public bool Contains(T item) => _stack.Contains(item);
+        public void Push(T item) => _stack.Add(item);
+        //Прибрати після реалізації IEnumerable
+        public void Show()
+        {
+            for(int i = _stack.Count - 1; i >= 0; i--)
+            {
+                Console.Write(_stack[i] + " "); 
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,6 +8,8 @@ class Program
         list.Add(1);
         list.Add(2);
         list.Add(3);
+        list.Add(1);
+        list.Add(2);
         Console.WriteLine(list.Count);
         list.Show();
         //list.Clear();
@@ -20,6 +22,16 @@ class Program
         //list.RemoveAt(2);
         //Console.WriteLine("-------------------------");
         //list.Show();
+        //list.Remove(1);
+        //list.Show();
+        List<int> list2 = new List<int>() {1, 2, 3, 4, 5, 6, 3};
+        //list.Reverse();
+        //list.Show();
+        Console.WriteLine(list2.LastIndexOf(3));
+        Console.WriteLine(list.LastIndexOf(5));
+        list.Show();
+        list.Insert(0, 1);
+        list.Show();
     }
 }
 
